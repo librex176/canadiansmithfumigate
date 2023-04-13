08a2e98930e8c2a7ab62cee4695a9d756a24a037
David <a17100641@6gmail.com>
2023-04-12 21:58:43 -0600

    Pagina

---
+++ alumnos.js
@@ -0,0 +1,17 @@
+export const alumnos = [
+    {
+        "nombre": "Wally A. Broderick E.",
+        "texto_informativo": "Humanista, Comunista, trabajando siempre por economías solidarias. Fundador de La Roja, cervecería para la inclusión social, y colaborador en otros proyectos de impacto social. Estudia filosofía budista como herramienta de desarrollo y es profesor de inglés. Ciudadano del mundo Colombo-Irlandés. Twitter: @larojacerveza"
+    },
+    {
+        "nombre": "Carlos Amador Martinez Ventura",
+        "texto_informativo": "Estudiante en la Orden Hsu Yun del Budismo Chan/Zen y Licenciado con el título de Médico cirujano por la Universidad Veracruzana."
+    },
+    {
+        "nombre": "Juan Camilo",
+        "texto_informativo": "Politólogo, Especialista en Justicia, Víctimas y Construcción de Paz y Maestrante en Políticas Públicas, todo ello en la Universidad Nacional de Colombia. Joven investigador con interés en  la Construcción de Paz, Conflicto Armado, Políticas Públicas y Estudios de Juventud. Experiencia en producción de conocimiento tanto en espacios académicos, como autónomos de las organizaciones populares; planificación estratégica desde organizaciones de economía social y solidaria, además de diversos espacios de educación popular y memoria histórica. La última publicación fue: 'Producción social de la ruta de reincorporación de las FARC-EP, subsunción sistémica y reproducción de la conflictividad armada' (2022)"
+        
+    }
+];
+
+
